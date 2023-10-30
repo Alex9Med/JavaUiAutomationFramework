0cd52154682535ead53f6ddea97975d42385ce67
Alex9Med <medoniialex@gmail.com>
2023-10-30 13:04:44 +0200

    Integrated the Cucumber tool

---
+++ src/main/java/com/opencart/stepdefinitions/RegisterPageSteps.java
@@ -0,0 +1,26 @@
+package com.opencart.stepdefinitions;
+
+import com.opencart.managers.DriverManager;
+import com.opencart.managers.RandomDataManager;
+import com.opencart.pageobjects.RegisterPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class RegisterPageSteps {
+    WebDriver driver = DriverManager.getInstance().getDriver();
+    RegisterPage registerPage = new RegisterPage(driver);
+
+    @When("the register form is populated with valid random data")
+    public void theRegisterFormIsPopulatedWithValidRandomData() {
+        String randomEmail = RandomDataManager.generateRandomEmail();
+        String password = RandomDataManager.generatePassword();
+
+        System.out.println(randomEmail);
+        System.out.println(password);
+
+        registerPage.fillInTheRegisterForm(RandomDataManager.generateFirstName(), RandomDataManager.generateLastName(), randomEmail, password, true);
+        System.out.println("The register form is populated with random data");
+    }
+
+}
